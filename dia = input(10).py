1b2a4f39176d557165c111b467eef97a73a96e7b
diego bojczuk <dbcbojczuk@gmail.com>
2025-09-24 21:46:33 -0300

    24092025

---
+++ dia = input(10).py
@@ -0,0 +1,8 @@
+dia = 10
+mes = 9
+ano = 1984
+print(dia, mes, ano)
+
+data = dia, mes, ano
+print(data)
+
---
+++ x = 'Rio Grande do Norte'.py
@@ -0,0 +1,9 @@
+lista = [1, 2, 3, 4, 5]
+lista2 = [10*i for i in lista]
+print(lista2)
+
+lista3 = []
+for i in lista:
+    lista3.append(10*i)
+print(lista3)
+
